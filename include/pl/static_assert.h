e50c6a9640e19c3d6e1ffe8b955b5703d4c3488f
Eczbek <eczbek.void@gmail.com>
2026-09-05 16:56:14 +0200

    Ignore GCC warning

--- include/pl/static_assert.h
+++ include/pl/static_assert.h
@@ -2,6 +2,9 @@
 #define PL_STATIC_ASSERT_H
 
 #ifndef __clang__
+ #ifdef __GNUC__
+  #pragma GCC diagnostic ignored "-Wunused-value" // https://gcc.gnu.org/bugzilla/show_bug.cgi?id=127192
+ #endif
  #define pl_static_assert(...) static_assert(__VA_ARGS__)
 #else
  #define pl_static_assert(...) ((void)(struct { static_assert(__VA_ARGS__); int defined; }){})
